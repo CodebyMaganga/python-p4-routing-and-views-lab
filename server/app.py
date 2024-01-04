106f251c246be3c6292cf447c036691562c15287
Maganga Mwambonu <codebymaganga@gmail.com>
2024-01-04 10:47:17 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -4,5 +4,41 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<hello>')
+def printme(hello):
+    print(f'{hello}')
+    return f'{hello}'
+
+@app.route('/count/<num>')
+def countme(num):
+    init = ""
+    for i in range(int(num)):
+        init += str(i) + '\n'
+    return init
+
+@app.route('/math/<num>/<operation>/<num2>')
+def mathit(num, operation, num2):
+    if operation == '+':
+        x=int(num) + int(num2)
+        return str(x)
+    elif operation == '-':
+        x=int(num) - int(num2)
+        return str(x)
+    elif operation == 'div':
+        x=int(num) / int(num2)
+        return str(x)
+    elif operation == '*':
+        x=int(num) * int(num2)
+        return str(x)
+    elif operation == '%':
+        x=int(num) % int(num2)
+        return str(x)
+
+    
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
